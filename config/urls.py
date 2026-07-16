dc8b168226a6ced2c720ae7e638e6d96069b0b25
maljedaili <83273856+maljedaili@users.noreply.github.com>
2026-07-16 06:53:11 +0200

    Serve kitchen app manifest at root

--- config/urls.py
+++ config/urls.py
@@ -4,14 +4,17 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 from shop.sitemaps import ProductSitemap, BlogSitemap, StaticSitemap
+from shop import views as shop_views
 
 sitemaps = {'static': StaticSitemap, 'products': ProductSitemap, 'blog': BlogSitemap}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('robots.txt', include('shop.urls_robots')), 
-    path('', include('shop.urls')),
+    path('manifest.webmanifest', shop_views.manifest_webmanifest, name='manifest_webmanifest'),
+    path('sw.js', shop_views.service_worker, name='service_worker'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('', include('shop.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
